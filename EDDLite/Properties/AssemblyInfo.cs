0687ef18679ad22896a85a4972ecac79d242a5d7
robbyxp1 <robxp@hotmail.com>
2025-05-01 14:52:44 +0100

    3.3.0

--- EDDLite/Properties/AssemblyInfo.cs
+++ EDDLite/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robby & EDDLite Team")]
 [assembly: AssemblyProduct("EDDLite")]
-[assembly: AssemblyCopyright("Copyright © Robby and the EDD Team 2015-2024")]
+[assembly: AssemblyCopyright("Copyright © Robby and the EDD Team 2015-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below :
-[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyVersion("3.3.0.0")]
